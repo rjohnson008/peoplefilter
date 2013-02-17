ff9ecb2f6c0b7370b6939add8a761a34b8587311
Ryan Johnson <rjohnson008@yahoo.com>
2013-02-17 10:16:59 -0800

    Initial commit

---
+++ Gemfile
@@ -0,0 +1,20 @@
+source 'https://rubygems.org'
+
+gem 'rails', '3.2.11'
+gem 'jquery-rails'
+
+
+gem :development do 
+  gem 'sqlite3'
+end
+
+gem :production, :test do
+  gem 'pg'
+end
+
+
+group :assets do
+  gem 'sass-rails',   '~> 3.2.3'
+  gem 'coffee-rails', '~> 3.2.1'
+  gem 'uglifier', '>= 1.0.3'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Peoplefilter::Application.config.secret_token = '23143ad104d5f7305797edf8da6d4cbbea82904b3c7974567bd9c7c3cddf3824dcf799fa2ac8ce6c0716f117409ff974d23af9898a2f21170d4fb27a6b1404a4'
