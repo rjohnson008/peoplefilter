be556aa8beef0e5590a99095aaeffd0f0bd6fe8c
Ryan Johnson <rjohnson008@yahoo.com>
2013-02-19 16:41:32 -0800

    Add Devise and  Simple Form

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,4 +64,7 @@ Peoplefilter::Application.configure do
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
+
+  # Default URL  for Devise. Set to the host URL before releasing to production.
+  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
 end
